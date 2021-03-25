d2e000587cca3d93c173d42ece685906321ec5ea
yemicodes <yemicodes@gmail.com>
2021-03-25 13:43:31 +0300

    handle patch request

--- .history/app_20210325123504.js
+++ .history/app_20210325123504.js
@@ -68,21 +68,7 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
-app.patch('/api/v1/tours/:id', (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid id',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
-});
+app.patch('/api/v1/tours/:id', (req, res) => {});
 
 const port = 3000;
 
--- .history/app_20210325133736.js
+++ .history/app_20210325133736.js
@@ -69,13 +69,12 @@ app.post('/api/v1/tours', (req, res) => {
 });
 
 app.patch('/api/v1/tours/:id', (req, res) => {
-  if (req.params.id * 1 > tours.length) {
+  if (!tour) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid id',
     });
   }
-
   res.status(200).json({
     status: 'success',
     data: {
