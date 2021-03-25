80a9433e0aeb8b342b9248d743bb5fe2a419611f
yemicodes <yemicodes@gmail.com>
2021-03-25 08:59:35 +0300

    started api
    handled get request

--- .history/app_20210325085456.js
+++ .history/app_20210325085456.js
@@ -11,16 +11,15 @@ const app = express();
 //   res.send('You can post to this endpoint....');
 // });
 
-const tours = JSON.parse(
+const tours = JSON.stringify.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
-    results: tours.length,
     data: {
-      tours,
+      tours: tours,
     },
   });
 });
