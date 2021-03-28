407abaa820567c244b51e8ed3588921218b360b7
yemicodes <yemicodes@gmail.com>
2021-03-28 14:38:30 +0300

    Update tourController.js
    advance filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,22 +2,29 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // build query
+    // .1 filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // .2 advance filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // execute query
+
+    const tours = await query;
 
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    // execute query
-
-    const tours = await query;
 
     // send response
     res.status(200).json({
