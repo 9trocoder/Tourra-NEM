b4320b336429a72f5752f82662e7a43a9bf44874
yemicodes <yemicodes@gmail.com>
2021-03-29 08:54:49 +0300

    limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,6 +24,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // .4 field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields); // projecting
+    } else {
+      query = query.select('-__v');
+    }
+
     // execute query
 
     const tours = await query;
