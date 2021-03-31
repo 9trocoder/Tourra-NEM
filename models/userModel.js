720c0c0bbcf27bed6b0832a53095b986a0487b54
yemicodes <yemicodes@gmail.com>
2021-03-31 23:19:11 +0300

    create new user

--- models/userModel.js
+++ models/userModel.js
@@ -2,17 +2,27 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please enter your name']
-    },
-    email: {
-        type: String,
-        required: [true, 'Please enter your email'],
-        unique: true,
-        lowercase: true,
-        validate: [validator.isEmail]
-    }
+  name: {
+    type: String,
+    required: [true, 'Please enter your name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please enter a valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'please confirm your password']
+  }
 });
 
 const User = mongoose.model('User', userSchema);
