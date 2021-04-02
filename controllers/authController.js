5798a621c0c1d82dc5e554b92df431e5b701df77
yemicodes <yemicodes@gmail.com>
2021-04-03 02:23:26 +0300

    authorization
    authorizing user, admin, guide, lead-guide roles and permission

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangedAt
+    passwordChangedAt: req.body.passwordChangedAt,
+    role: req.body.role
   });
 
   const token = signToken(newUser._id);
@@ -90,3 +91,16 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles is an array ['admin', 'lead-guide']
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('you do not have permission to perform this action', 403)
+      );
+    }
+
+    next();
+  };
+};
