f6093363e711a4f8fd51eac23a57f76716e88474
yemicodes <yemicodes@gmail.com>
2021-04-05 20:19:44 +0300

    preventing parameter pollution

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -15,7 +16,7 @@ const app = express();
 
 // global middleware
 // security http header
-app.use(helmet())
+app.use(helmet());
 
 // development logging
 if (process.env.NODE_ENV === 'development') {
@@ -32,17 +33,32 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // body parser, reading data from body into req.body
-app.use(express.json({
-  limit: '10kb'
-}));
+app.use(
+  express.json({
+    limit: '10kb'
+  })
+);
 
 // data sanitation against nosql query injection
 app.use(mongoSanitize());
 // data sanitation against xss
-app.use(xss())
+app.use(xss());
+
+// prevents parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 // serving static files
 app.use(express.static(`${__dirname}/public`));
-
 
 // testing middleware
 app.use((req, res, next) => {
