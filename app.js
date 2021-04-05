d2cbfc528a166f6157c76e0496ac7a133bb87bcc
yemicodes <yemicodes@gmail.com>
2021-04-05 18:41:26 +0300

    sanitizing data's

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -34,6 +36,10 @@ app.use(express.json({
   limit: '10kb'
 }));
 
+// data sanitation against nosql query injection
+app.use(mongoSanitize());
+// data sanitation against xss
+app.use(xss())
 // serving static files
 app.use(express.static(`${__dirname}/public`));
 
