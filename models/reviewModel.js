45612fd0861b14dd0f1fdbe099439ad8b79cc080
yemicodes <yemicodes@gmail.com>
2021-04-06 16:33:44 +0300

    Create reviewModel.js
    modeeling reviews as parent referencing

---
+++ models/reviewModel.js
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Tour must have review']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'User must have review']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
