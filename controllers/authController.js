122bca11d22739d29d90f3a1903febb25ae76bf3
yemicodes <yemicodes@gmail.com>
2021-04-11 03:41:05 +0300

    login out user
    continuation of login user and logout user using the api

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,6 +64,14 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+  res.status(200).json({ status: 'success' });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
   // get token and check if it exist
   let token;
@@ -105,6 +113,35 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+// only for rendered pages, no errors!
+exports.isLoggedIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      // verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
+      // check if user exist
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
+      //check if user change password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+
+      // there is a logged in user
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
+    }
+  }
+  next();
+};
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
-
-const login = async (email, password) => {
-  console.log(email, password);
+import axios from 'axios';
+import { showAlert } from './alert';
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -11,15 +11,26 @@ const login = async (email, password) => {
         password
       }
     });
-    console.log(res);
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Logged in succesfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
   } catch (err) {
-    console.log(err.response.data);
+    showAlert('error', err.response.data.message);
   }
 };
 
-document.querySelector('.form').addEventListener('submit', e => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://localhost:8000/api/v1/users/logout'
+    });
+    if ((res.data.status = 'success')) location.reload(true);
+  } catch (err) {
+    showAlert('error', 'Error logging out, Try again!');
+  }
+};
